7abea24e9c7bbf1491fad1d2d40f8006442abb23
sj97 <jshreya97@yahoo.com>
2017-07-16 10:52:08 +0530

    DOB format change

--- education/models.py
+++ education/models.py
@@ -29,7 +29,7 @@ class Education(models.Model):
     # PERSONAL DETAILS
     name = models.CharField(max_length=100, default='')
     homeadd = models.CharField(max_length=200, default='N/A')
-    dob = models.DateField(default='2000-1-1')
+    dob = models.DateField(default='2000-12-20')
 
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     contact = models.CharField(validators=[phone_regex], blank=True, max_length=15) # validators should be a list
